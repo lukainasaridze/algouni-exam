94b1fee5b59ac828e6da39571c8997eaa6d70bba
luka <geojer36@gmail.com>
2025-10-08 09:34:38 +0400

    Add GitHub Actions CI workflow

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,7 +9,7 @@ def test_get_form():
     assert "სანომრე ნიშნების ვალიდაცია" in response.text
 
 def test_is_valid():
-    from main import is_valid
+    from ..main import is_valid
     assert is_valid("AB-123-CDE") == True
     assert is_valid("ab-123-cde") == False  # lowercase
     assert is_valid("AB123CDE") == False  # no dashes
