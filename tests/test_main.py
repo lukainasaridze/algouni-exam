54035cc1f7415c402bc2d5cae2e187974cd76a5d
luka <geojer36@gmail.com>
2025-10-08 09:40:01 +0400

    Add GitHub Actions CI workflow

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from ..main import app  # ორი წერტილი, რადგან tests ძირეულიდან ერთი დონით ქვემოთაა
+from main import app  # ორი წერტილი, რადგან tests ძირეულიდან ერთი დონით ქვემოთაა
 
 client = TestClient(app)
 
@@ -9,7 +9,7 @@ def test_get_form():
     assert "სანომრე ნიშნების ვალიდაცია" in response.text
 
 def test_is_valid():
-    from ..main import is_valid
+    from main import is_valid
     assert is_valid("AB-123-CDE") == True
     assert is_valid("ab-123-cde") == False  # lowercase
     assert is_valid("AB123CDE") == False  # no dashes
